0326fd45228af5ba2426e227f51242e0ffabd031
moonou <moonoulong@gmail.com>
2018-02-02 17:05:30 +0800

    fetch books data from leancloud

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import AV from 'leancloud-storage'
+
+import leancloudConfig from '@/assets/leancloud.json'
 
 import 'normalize.css'
 import '@/assets/styles/app.scss'
+import '@/assets/styles/animation.scss'
+
+const appId = leancloudConfig.id
+const appKey = leancloudConfig.key
+
+AV.init({ appId, appKey })
+
+window.AV = AV
 
 Vue.config.productionTip = false
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="home">
+
+    <transition name="fade">
+      <div class="cover-bg" v-if="background.visible">
+        <img :src="background.cover" alt="">
+      </div>
+    </transition>
+
     <div class="container">
       <div class="title">
         列表🦉
       </div>
 
       <div class="list">
-        <div class="item">
-          <div class="cover"></div>
-          <div class="name">七大罪</div>
+        <div
+          class="item"
+          v-for="(book, index) in list"
+          :key="index"
+          @mouseenter="mEnter(book)"
+          @mouseout="mOut(book)"
+        >
+          <div class="cover">
+            <img :src="book.cover" alt="">
+          </div>
+          <div class="name">
+            {{ book.name }}
+          </div>
         </div>
       </div>
     </div>
@@ -16,8 +33,38 @@
 </template>
 
 <script>
-export default {
 
+export default {
+  data () {
+    return {
+      list: [],
+      background: {
+        cover: '',
+        visible: false
+      }
+    }
+  },
+  created () {
+    let query = new AV.Query('book')
+    query.find().then((data) => {
+      this.list = data.map((book) => {
+        return {
+          name: book.get('name'),
+          cover: book.get('cover'),
+          id: book.id
+        }
+      })
+    })
+  },
+  methods: {
+    mEnter (book) {
+      this.background.visible = true
+      this.background.cover = book.cover
+    },
+    mOut () {
+      this.background.visible = false
+    }
+  }
 }
 </script>
 
@@ -31,8 +78,23 @@ export default {
   justify-content: center;
   align-items: center;
 
+  .cover-bg {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      filter: blur(40px);
+    }
+  }
+
   .container {
     width: 70%;
+    position: relative;
+    z-index: 2;
 
     .title {
       font-size: 50px;
@@ -42,21 +104,43 @@ export default {
     .list {
       margin-top: 40px;
       text-align: center;
+      white-space: nowrap;
+      width: 100%;
+      overflow-x: scroll;
+      padding: 50px 0;
 
       .item {
         width: 200px;
         display: inline-block;
         margin: 0 20px;
+        &:hover {
+          .cover {
+            transform: translateY(-10px);
+            box-shadow: 0 0 15px 0 #666666;
+          }
+        }
+
         .cover {
           width: 200px;
           height: 250px;
           background-color: #353535;
           border-radius: 15px;
           cursor: pointer;
+          overflow: hidden;
+          transition: all .3s ease;
+          box-shadow: 0 0 5px 0 #8e8e8e;
+
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
         }
         .name {
           text-align: center;
           margin-top: 20px;
+          color: #444;
+          text-shadow: 0 0 1px #fff;
         }
       }
     }
